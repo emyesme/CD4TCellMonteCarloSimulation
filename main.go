f0c87bff5b888f2b8f72a30b1816440a6ca67d08
Adrian <aecheverri@truora.com>
2019-09-11 01:15:57 -0500

    graphics are fun

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
+	"os"
 	"time"
 	"univalle/sim/project/model"
+
+	chart "github.com/wcharczuk/go-chart"
 )
 
 const (
@@ -14,33 +16,94 @@ const (
 
 	pHIV = 0.005
 
-	runs = 10000
+	runs = 100
 )
 
 func main() {
-	globalOverview := model.NodeOverview{
-		Paramenters: map[model.State]float32{},
-		NodeSize:    lNodeSize,
+	globalYearsResume := []model.NodeOverview{}
+
+	for i := 0; i < 13; i++ {
+		globalYearsResume = append(globalYearsResume, model.NodeOverview{NodeSize: lNodeSize})
 	}
 
 	for i := 0; i < runs; i++ {
 		rand.Seed(time.Now().UnixNano())
 
 		lNode := model.NewInfectedLNode(rand.Float32, lNodeSize, pHIV)
-		// lNode.GetNodeOverview().Print()
-		fmt.Println()
+		yearsResume := lNode.Run(yearsInWeeks, rand.Float32)
 
-		lNode.Run(yearsInWeeks, rand.Float32)
-		localOverview := lNode.GetNodeOverview()
-
-		globalOverview.Paramenters[model.StateA0] = (globalOverview.Paramenters[model.StateA0]*(float32)(i) + localOverview.Paramenters[model.StateA0]) / (float32)(i+1)
-		globalOverview.Paramenters[model.StateA1] = (globalOverview.Paramenters[model.StateA1]*(float32)(i) + localOverview.Paramenters[model.StateA1]) / (float32)(i+1)
-		globalOverview.Paramenters[model.StateA2] = (globalOverview.Paramenters[model.StateA2]*(float32)(i) + localOverview.Paramenters[model.StateA2]) / (float32)(i+1)
-		globalOverview.Paramenters[model.StateT] = (globalOverview.Paramenters[model.StateT]*(float32)(i) + localOverview.Paramenters[model.StateT]) / (float32)(i+1)
-		globalOverview.Paramenters[model.StateD] = (globalOverview.Paramenters[model.StateD]*(float32)(i) + localOverview.Paramenters[model.StateD]) / (float32)(i+1)
-		globalOverview.Paramenters[model.StateEmpty] = (globalOverview.Paramenters[model.StateEmpty]*(float32)(i) + localOverview.Paramenters[model.StateEmpty]) / (float32)(i+1)
-
-		globalOverview.Print()
-		fmt.Println()
+		for j := 0; j < len(yearsResume); j++ {
+			globalYearsResume[j].A0 = (globalYearsResume[j].A0*float32(i) + yearsResume[j].A0) / float32(i+1)
+			globalYearsResume[j].A1 = (globalYearsResume[j].A1*float32(i) + yearsResume[j].A1) / float32(i+1)
+			globalYearsResume[j].A2 = (globalYearsResume[j].A2*float32(i) + yearsResume[j].A2) / float32(i+1)
+			globalYearsResume[j].T = (globalYearsResume[j].T*float32(i) + yearsResume[j].T) / float32(i+1)
+			globalYearsResume[j].D = (globalYearsResume[j].D*float32(i) + yearsResume[j].D) / float32(i+1)
+			globalYearsResume[j].Empty = (globalYearsResume[j].Empty*float32(i) + yearsResume[j].Empty) / float32(i+1)
+		}
 	}
+
+	xValues := []float64{}
+	ValuesT := []float64{}
+	ValuesA1PlusA2 := []float64{}
+	ValuesA0 := []float64{}
+	ValuesD := []float64{}
+	ValuesEmpty := []float64{}
+
+	for i := 0; i < len(globalYearsResume); i++ {
+		xValues = append(xValues, float64(i))
+		ValuesT = append(ValuesT, float64(globalYearsResume[i].T))
+		ValuesA1PlusA2 = append(ValuesA1PlusA2, float64(globalYearsResume[i].A1)+float64(globalYearsResume[i].A2))
+		ValuesA0 = append(ValuesA0, float64(globalYearsResume[i].A0))
+		ValuesD = append(ValuesD, float64(globalYearsResume[i].D))
+		ValuesEmpty = append(ValuesEmpty, float64(globalYearsResume[i].Empty))
+	}
+
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Name: "Años",
+		},
+		YAxis: chart.YAxis{
+			Name: "Conteo de células",
+		},
+		Series: []chart.Series{
+			chart.AnnotationSeries{
+				Annotations: []chart.Value2{
+					{XValue: 12.0, YValue: ValuesT[12], Label: "T"},
+					{XValue: 12.0, YValue: ValuesA1PlusA2[12], Label: "A1 + A2"},
+					{XValue: 12.0, YValue: ValuesA0[12], Label: "A0"},
+					{XValue: 12.0, YValue: ValuesD[12], Label: "D"},
+					{XValue: 12.0, YValue: ValuesEmpty[12], Label: "Espacios vacíos"},
+				},
+			},
+			chart.ContinuousSeries{
+				Name:    "T",
+				XValues: xValues,
+				YValues: ValuesT,
+			},
+			chart.ContinuousSeries{
+				Name:    "A1 + A2",
+				XValues: xValues,
+				YValues: ValuesA1PlusA2,
+			},
+			chart.ContinuousSeries{
+				Name:    "A0",
+				XValues: xValues,
+				YValues: ValuesA0,
+			},
+			chart.ContinuousSeries{
+				Name:    "D",
+				XValues: xValues,
+				YValues: ValuesD,
+			},
+			chart.ContinuousSeries{
+				Name:    "0",
+				XValues: xValues,
+				YValues: ValuesEmpty,
+			},
+		},
+	}
+
+	f, _ := os.Create("output.png")
+	defer f.Close()
+	graph.Render(chart.PNG, f)
 }
